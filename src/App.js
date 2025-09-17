e7fc34d325d4336b52a1b0efa8949ca88f249af4
avi-codesmith <avi.codesmith@gmail.com>
2025-09-17 22:39:50 +0530

    My first React app with different components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import prologo from "./pro-logo.svg";
+import "./App.css";
+
+const strings = ["Avi-codesmith", "a Fresher", "a New bie", "a Student"];
+const random = Math.floor(Math.random() * strings.length);
+
+const data = {
+  name: "Scott Churrner",
+  work: "Teacher",
+  id: "@1928Ow7G",
+};
+
+function Header() {
+  return (
+    <div className="container">
+      <img src={prologo} className="img" alt="pro-logo" />
+      <p class="pro-para">Warm React Java Script By {strings[random]}</p>
+    </div>
+  );
+}
+
+function Box() {
+  return (
+    <div class="component">
+      <h1>{data.name}</h1>
+      <h3>{data.work}</h3>
+      <p>
+        Work is {data.work} and the ID is "{data.id}"
+      </p>
+      <button>follow me!</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +51,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Box />
     </div>
   );
 }
 
-export default App;
+export default App; // its a file name "App" not function
